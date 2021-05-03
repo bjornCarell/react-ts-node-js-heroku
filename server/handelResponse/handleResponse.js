48f800ac48905a6b2446bcdb57b7c47570a06d34
Bjorn Carell <bjorn.carell@tink.com>
2021-05-03 08:54:18 +0200

    chore(server): test-setup

--- server/handelResponse/handleResponse.js
+++ server/handelResponse/handleResponse.js
@@ -1,8 +1,8 @@
-export const handleResponse = async response => {
-    const json = await response.json();
-    
-    if (response.status !== 200) {
-      throw new Error(json.errorMessage);
-    }
-    return json;
-}
+export const handleResponse = async (response) => {
+  const json = await response.json();
+
+  if (response.status !== 200) {
+    throw new Error(json.errorMessage);
+  }
+  return json;
+};
